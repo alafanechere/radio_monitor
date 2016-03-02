9b79731e8442748771371b17f8621bc13f98634f
augustin <augustin@Augustin-2.local>
2016-03-02 18:54:43 +0100

    create basic threading

--- radio_monitor/main.py
+++ radio_monitor/main.py
@@ -1,30 +1,32 @@
 import collectors
+import time
+from threading import Thread
+
+
+class Pinger(Thread):
+
+    def __init__(self, collector):
+        Thread.__init__(self)
+        self.collector = collector
+
+    def run(self):
+        while True:
+            meta = self.collector.get_current_metadata()
+            print self.collector.RADIO_NAME
+            print meta
+            print "--------"
+            time.sleep(self.collector.crawl_frequency * 60)
 
 
 def main():
-    fip_collector = collectors.FipCollector(1)
-    meta = fip_collector.get_current_metadata()
-    print meta
+    fip_thread = Pinger(collectors.FipCollector())
+    nova_thread = Pinger(collectors.NovaCollector())
 
-    print "------------"
-    nova_collector = collectors.NovaCollector(1)
-    meta = nova_collector.get_current_metadata()
-    print meta
+    fip_thread.start()
+    nova_thread.start()
 
-    print "------------"
-    fun_radio_collector = collectors.FunRadioCollector()
-    meta = fun_radio_collector.get_current_metadata()
-    print meta
-
-    print "------------"
-    nrj_collector = collectors.NrjCollector()
-    meta = nrj_collector.get_current_metadata()
-    print meta
-
-    print "------------"
-    skyrock_collector = collectors.SkyrockCollector()
-    meta = skyrock_collector.get_current_metadata()
-    print meta
+    fip_thread.join()
+    nova_thread.join()
 
 if __name__ == '__main__':
     main()
