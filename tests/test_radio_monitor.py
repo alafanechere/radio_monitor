4773a95a3fcfeafb7101177829a2d5103c7dfc32
augustin <augustin@Augustin-2.local>
2016-03-02 14:27:08 +0100

    test metadata for nova

--- tests/test_radio_monitor.py
+++ tests/test_radio_monitor.py
@@ -16,3 +16,16 @@ def test_metadata_fip():
     assert isinstance(current_metadata.broadcaster, str)
     assert isinstance(current_metadata.broadcast_time, datetime.datetime)
 
+
+def test_metadata_nova():
+    nova_collector = core.NovaCollector(1)
+    current_metadata = nova_collector.get_current_metadata()
+
+    assert isinstance(current_metadata.title, unicode)
+    assert isinstance(current_metadata.artist, unicode)
+    assert current_metadata.album is None
+    assert current_metadata.label is None
+    assert current_metadata.year is None
+    assert isinstance(current_metadata.broadcaster, str)
+    assert isinstance(current_metadata.broadcast_time, datetime.datetime)
+
