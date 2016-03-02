acf43effdaf003d83c6dc4c994657f524bbc3a3f
augustin <augustin@Augustin-2.local>
2016-03-02 11:38:45 +0100

    enrich metadata parsing test

--- tests/test_radio_monitor.py
+++ tests/test_radio_monitor.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+import datetime
 import pytest
 from radio_monitor import core
 
 
-def test_answer():
+def test_metadata_parsing():
     fip_collector = core.FipCollector(1)
     current_metadata = fip_collector.get_current_metadata()
-    assert current_metadata is not None
+
+    assert isinstance(current_metadata.title, unicode)
+    assert isinstance(current_metadata.artist, unicode)
+    assert isinstance(current_metadata.broadcaster, str)
+    assert isinstance(current_metadata.broadcast_time, datetime.datetime)
+
