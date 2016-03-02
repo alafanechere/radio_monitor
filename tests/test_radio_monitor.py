fd9db125d988e5c3805ee8b8b48e8d5a14669cad
augustin <augustin@Augustin-2.local>
2016-03-02 12:00:58 +0100

    refactor test_metadata_parsing to test_metadata_fip

--- tests/test_radio_monitor.py
+++ tests/test_radio_monitor.py
@@ -4,12 +4,15 @@ import pytest
 from radio_monitor import core
 
 
-def test_metadata_parsing():
+def test_metadata_fip():
     fip_collector = core.FipCollector(1)
     current_metadata = fip_collector.get_current_metadata()
 
     assert isinstance(current_metadata.title, unicode)
     assert isinstance(current_metadata.artist, unicode)
+    assert isinstance(current_metadata.album, unicode)
+    assert isinstance(current_metadata.label, unicode)
+    assert isinstance(current_metadata.year, int)
     assert isinstance(current_metadata.broadcaster, str)
     assert isinstance(current_metadata.broadcast_time, datetime.datetime)
 
