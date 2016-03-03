aaf055563e19fd2f683e63a4a474a9e187a5100a
augustin <augustin@Augustin-2.local>
2016-03-03 17:26:41 +0100

    get current track outside of threads

--- radio_monitor/main.py
+++ radio_monitor/main.py
@@ -8,21 +8,19 @@ class Pinger(threading.Thread):
     def __init__(self, collector):
         super(Pinger, self).__init__()
         self.collector = collector
+        self.current_meta = None
         self._stop = threading.Event()
 
     def run(self):
-        current_meta = None
         while self.stopped() is False:
             meta = self.collector.get_current_metadata()
 
             try:
-                if current_meta is None or (current_meta.title != meta.title and current_meta.artist != meta.artist):
-                    current_meta = meta
-                    print self.collector.RADIO_NAME
-                    print meta
-                    print "--------"
+                if self.current_meta is None or (self.current_meta.title != meta.title
+                                                 and self.current_meta.artist != meta.artist):
+                    self.current_meta = meta
             except AttributeError:
-                current_meta = None
+                self.current_meta = None
 
             time.sleep(self.collector.crawl_frequency)
 
@@ -32,29 +30,44 @@ class Pinger(threading.Thread):
     def stopped(self):
         return self._stop.isSet()
 
-
 def main():
-    threads = [Pinger(collectors.FipCollector()),
-               Pinger(collectors.NovaCollector()),
-               Pinger(collectors.NrjCollector()),
-               Pinger(collectors.FunRadioCollector()),
-               Pinger(collectors.SkyrockCollector())]
+    threads = {"FIP": Pinger(collectors.FipCollector()),
+               "Nova": Pinger(collectors.NovaCollector()),
+               "NRJ": Pinger(collectors.NrjCollector()),
+               "Fun Radio": Pinger(collectors.FunRadioCollector()),
+               "Skyrock": Pinger(collectors.SkyrockCollector())}
 
-    for radio_thread in threads:
+    for _, radio_thread in threads.iteritems():
         radio_thread.start()
+
+    time.sleep(2)
+
+    current_metas = {}
+
+    for radio, pinger in threads.iteritems():
+        current_metas[radio] = pinger.current_meta
+
+    for radio, meta in current_metas.iteritems():
+        print radio
+        print meta
 
     on = True
     while on:
         try:
-            pass
+            time.sleep(1)
+            for radio, meta in current_metas.iteritems():
+                if meta != threads[radio].current_meta:
+                    current_metas[radio] = threads[radio].current_meta
+                    print current_metas[radio]
+                    print "\n\n"
         except KeyboardInterrupt:
             on = False
 
-    for radio_thread in threads:
-        radio_thread.stop()
+    for _, thread in threads.iteritems():
+        thread.stop()
 
-    for radio_thread in threads:
-        radio_thread.join()
+    for _, thread in threads.iteritems():
+        thread.join()
 
 if __name__ == '__main__':
     main()
